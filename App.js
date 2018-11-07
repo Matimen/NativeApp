634f4fa66bb31112222d5252a6a391c0251bff91
Mateusz Bem <mateusz.bem@asseco.pl>
2018-11-07 14:24:24 +0100

    Side menu try

--- App.js
+++ App.js
@@ -1,21 +1,14 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import {Text, View} from 'react-native';
+import {styles} from "src/styles/app-styles";
+import SideMenu from "src/components/side-menu/side-menu";
 
 export default class App extends React.Component {
-  render() {
-    return (
-      <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
-      </View>
-    );
-  }
+    render() {
+        return (
+            <View style={styles.container}>
+                <SideMenu/>
+            </View>
+        );
+    }
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
