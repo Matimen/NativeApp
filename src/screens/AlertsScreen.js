9ba677125d985bcbbbb408983cb81c600dc7368e
ACP\Mateusz.Miasik <mateusz.miasik@asseco.pl>
2018-11-08 14:49:22 +0100

    Alerty

--- src/screens/AlertsScreen.js
+++ src/screens/AlertsScreen.js
@@ -1,17 +1,24 @@
-import React, { Component } from 'react';
-import { Table, TableWrapper, Row } from 'react-native-table-component';
-import {Icon} from "react-native-elements";
+import React, {Component} from 'react';
+import {Table, TableWrapper, Row} from 'react-native-table-component';
+import {Icon, Card, ListItem} from "react-native-elements";
 import {
     View, Text, StyleSheet, TouchableOpacity, Button, ScrollView
 } from 'react-native';
 import DrawerIcon from '../components/menu-icon/menu-icon';
+const users = [
+    {
+        name: 'brynn',
+        avatar: 'https://s3.amazonaws.com/uifaces/faces/twitter/brynn/128.jpg'
+    },
 
+];
 
 export default class AlertsScreen extends Component {
     static navigationOptions = {
         drawerLabel: 'Alerty',
-        drawerIcon: ({ tintColor }) => (<Icon name={'notifications'} color={tintColor}/>  ),
+        drawerIcon: ({tintColor}) => (<Icon name={'notifications'} color={tintColor}/>),
     };
+
     constructor(props) {
         super(props);
         this.state = {
@@ -19,6 +26,7 @@ export default class AlertsScreen extends Component {
             widthArr: [40, 60, 80, 100, 120, 140, 160, 180, 200]
         }
     }
+
     render() {
         const state = this.state;
         const tableData = [];
@@ -31,39 +39,57 @@ export default class AlertsScreen extends Component {
         }
 
         return (
-            <View style={styles.container}>
-                <ScrollView horizontal={true}>
-                    <View>
-                        <Table borderStyle={{borderColor: '#C1C0B9'}}>
-                            <Row data={state.tableHead} widthArr={state.widthArr} style={styles.header} textStyle={styles.text}/>
-                        </Table>
-                        <ScrollView style={styles.dataWrapper}>
-                            <Table borderStyle={{borderColor: '#C1C0B9'}}>
+
+                <View style={styles.container}>
+
+                    <ScrollView horizontal={true}>
+                        <View>
+                            <Card containerStyle={{padding: 0}} >
                                 {
-                                    tableData.map((rowData, index) => (
-                                        <Row
-                                            key={index}
-                                            data={rowData}
-                                            widthArr={state.widthArr}
-                                            style={[styles.row, index%2 && {backgroundColor: '#F7F6E7'}]}
-                                            textStyle={styles.text}
-                                        />
-                                    ))
+                                    users.map((u, i) => {
+                                        return (
+                                            <ListItem
+                                                key={i}
+                                                roundAvatar
+                                                title={u.name}
+                                                avatar={{uri:u.avatar}}
+                                            />
+                                        );
+                                    })
                                 }
+                            </Card>
+                            <Table borderStyle={{borderColor: '#C1C0B9'}}>
+                                <Row data={state.tableHead} widthArr={state.widthArr} style={styles.header}
+                                     textStyle={styles.text}/>
                             </Table>
-                        </ScrollView>
-                    </View>
-                </ScrollView>
-            </View>
+                            <ScrollView style={styles.dataWrapper}>
+                                <Table borderStyle={{borderColor: '#C1C0B9'}}>
+                                    {
+                                        tableData.map((rowData, index) => (
+                                            <Row
+                                                key={index}
+                                                data={rowData}
+                                                widthArr={state.widthArr}
+                                                style={[styles.row, index % 2 && {backgroundColor: '#F7F6E7'}]}
+                                                textStyle={styles.text}
+                                            />
+                                        ))
+                                    }
+                                </Table>
+                            </ScrollView>
+                        </View>
+                    </ScrollView>
+                </View>
+
         )
     }
 }
 
 const styles = StyleSheet.create({
-    container: { flex: 1, padding: 16, paddingTop: 30, backgroundColor: '#fff' },
-    header: { height: 50, backgroundColor: '#537791' },
-    text: { textAlign: 'center', fontWeight: '100' },
-    dataWrapper: { marginTop: -1 },
-    row: { height: 40, backgroundColor: '#E7E6E1' }
+    container: {flex: 1, padding: 16, paddingTop: 30, backgroundColor: '#fff'},
+    header: {height: 50, backgroundColor: '#537791'},
+    text: {textAlign: 'center', fontWeight: '100'},
+    dataWrapper: {marginTop: -1},
+    row: {height: 40, backgroundColor: '#E7E6E1'}
 });
 
