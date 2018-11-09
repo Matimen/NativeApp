d25545c8e80fd4038040e497ef5934d34f60f808
ACP\Mateusz.Miasik <mateusz.miasik@asseco.pl>
2018-11-09 13:20:12 +0100

    styles and home

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -1,42 +1,58 @@
 import React, {Component} from 'react';
 import {
-    View, Text, StyleSheet, TouchableOpacity, Button
+    View, Text, StyleSheet, TouchableOpacity
 } from 'react-native';
 import DrawerIcon from '../components/menu-icon/menu-icon';
 import {Icon} from "react-native-elements";
-import { Container, Header, Content, Card, CardItem, Body } from 'native-base';
-
+import {Container, Header, Content, Card, CardItem, Body, Button} from 'native-base';
 
 export default class HomeScreen extends Component {
-     static navigationOptions = {
+    static navigationOptions = {
         drawerLabel: 'Strona Główna',
-        drawerIcon: ({ tintColor }) => (<Icon name={'notifications'} color={tintColor}/>  ),
+        drawerIcon: ({tintColor}) => (<Icon name={'notifications'} color={tintColor}/>),
     };
-    render(){
+
+    render() {
         return (
             <View>
                 <DrawerIcon name={'Strona Główna'}/>
                 <View>
+                    <Card>
+                        <CardItem header>
+                            <Text>ESIM - Event and Service Impact Manager</Text>
+                        </CardItem>
+                        <CardItem>
+                            <Body style={{flex: 1, flexDirection: 'row', justifyContent: 'space-between',}}>
+                            <Button success style={styles.btn}>
+                                <Text>Click Me!</Text>
+                            </Button>
+                            <Button info style={styles.btn}>
+                                <Text>Click !</Text>
+                            </Button>
+                            <Button warning style={styles.btn}>
+                                <Text>Click !</Text>
+                            </Button>
+                            <Button warning style={styles.btn}>
+                                <Text>Click !</Text>
+                            </Button>
+                            <Button danger style={styles.btn}>
+                                <Text>Click !</Text>
+                            </Button>
+                            <Button danger style={styles.btn}>
+                                <Text>Click !</Text>
+                            </Button>
+                            </Body>
+                        </CardItem>
 
-                            <Card>
-                                <CardItem header>
-                                    <Text>NativeBase</Text>
-                                </CardItem>
-                                <CardItem>
-                                    <Body>
-                                    <Text>
-                                        //Your text here
-                                    </Text>
-                                    </Body>
-                                </CardItem>
-                                <CardItem footer>
-                                    <Text>GeekyAnts</Text>
-                                </CardItem>
-                            </Card>
-                        
+                    </Card>
                 </View>
             </View>
         )
     }
 }
+const styles = StyleSheet.create({
+    btn: {
+        paddingHorizontal: 5,
+    }
+});
 
