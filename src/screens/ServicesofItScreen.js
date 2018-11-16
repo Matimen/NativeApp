be56a813e7c760a02fc433cb8ff0e3ca4bf4ea05
Mateusz.Bem <mateusz.bem@asseco.pl>
2018-11-16 10:29:41 +0100

    Code clearing

--- src/screens/ServicesofItScreen.js
+++ src/screens/ServicesofItScreen.js
@@ -1,114 +1,22 @@
 import React, {Component} from 'react';
-import {Table, Row, TableWrapper, Cell} from 'react-native-table-component';
-import {Icon, Container} from "native-base";
+import {Icon} from "native-base";
 import {
-    View, StyleSheet, ScrollView, ActivityIndicator, Text
+    View, Text
 } from 'react-native';
-import Moment from 'moment';
-import _ from 'lodash';
-import DrawerIcon from '../components/menu-icon/menu-icon';
-import TableFilters from "../components/filters/tableFilters";
-import {getData} from "../api/api";
 
 export default class AlertsScreen extends Component {
     static navigationOptions = {
-        drawerLabel: 'Alerty',
-        drawerIcon: ({tintColor}) => (<Icon name={'notifications'} color={tintColor}/>),
+        drawerLabel: 'Usługi IT',
+        drawerIcon: ({tintColor}) => (<Icon name={'desktop'} color={tintColor}/>),
     };
 
-    constructor(props) {
-        super(props);
-        this.state = {
-            idSorted: 'desc',
-            alertsData: [],
-            filteredData: [],
-            tableHead: [
-                <View><Text style={styles.text} onPress={()=>this.sortbyID('AlertId')}>ID</Text></View>,
-                'ApiKey',
-                'Nazwa Klienta',
-                'Data od',
-                'Data do'],
-            widthArr: [50, 30, 130, 100, 100]
-        }
-    }
-
-    componentDidMount() {
-        getData().then(response => {
-            this.setState({
-                alertsData: response.data,
-            });
-        });
-    }
-    sortbyID(column){
-        if (this.state.idSorted === 'desc'){
-            this.setState({
-                alertsData: _.orderBy(this.state.alertsData, column, 'asc'),
-                idSorted: 'asc'
-            })
-        } else {
-            this.setState({
-                alertsData: _.orderBy(this.state.alertsData, column, 'desc'),
-                idSorted: 'desc'
-            })
-        }
-    }
-    filterData(data) {
-        console.log(data);
-        this.setState({
-            filteredData: data
-        })
-    }
     render() {
         return (
             <View>
-                <DrawerIcon name={'Alerty'}/>
-                <View>
-                    <TableFilters alertsData={this.state.alertsData} filter={this.filterData.bind(this)}></TableFilters>
-                </View>
-                <View>
-                    <Table style={{borderColor: 'transparent', alignItems: 'center', justifyContent: 'center'}}>
-                        <Row data={this.state.tableHead} style={styles.head} textStyle={styles.text} widthArr={this.state.widthArr}/>
-                        {   this.state.filteredData.length === 0 ?
-                            (this.state.alertsData.length > 0 ?
-                                this.state.alertsData.map((rowData, index) => (
-                                    <TableWrapper key={index} style={[styles.row, index%2 && {backgroundColor: '#e3eefe'}]}>
-                                        <Cell data={rowData.AlertId} textStyle={styles.text}
-                                              width={this.state.widthArr[0]}/>
-                                        <Cell data={rowData.ClientApiKeyId} textStyle={styles.text}
-                                              width={this.state.widthArr[1]}/>
-                                        <Cell data={rowData.ClientName} textStyle={styles.text}
-                                              width={this.state.widthArr[2]}/>
-                                        <Cell data={Moment(rowData.StartDate).format('DD.MM.YYYY')}
-                                              textStyle={styles.text} width={this.state.widthArr[3]}/>
-                                        <Cell data={Moment(rowData.StopDate).format('DD.MM.YYYY')}
-                                              textStyle={styles.text} width={this.state.widthArr[4]}/>
-                                    </TableWrapper>
-                                )) : <ActivityIndicator style={{alignItems: 'flex-start', marginBottom: 50, marginTop: 50}} size="large" color="#0000ff" />)
-                            :
-                            this.state.filteredData.map((rowData, index) => (
-                                <TableWrapper key={index} style={[styles.row, index%2 && {backgroundColor: '#e3eefe'}]}>
-                                    <Cell data={rowData.AlertId} textStyle={styles.text}
-                                          width={this.state.widthArr[0]}/>
-                                    <Cell data={rowData.ClientApiKeyId} textStyle={styles.text}
-                                          width={this.state.widthArr[1]}/>
-                                    <Cell data={rowData.ClientName} textStyle={styles.text}
-                                          width={this.state.widthArr[2]}/>
-                                    <Cell data={Moment(rowData.StartDate).format('DD.MM.YYYY')}
-                                          textStyle={styles.text} width={this.state.widthArr[3]}/>
-                                    <Cell data={Moment(rowData.StopDate).format('DD.MM.YYYY')}
-                                          textStyle={styles.text} width={this.state.widthArr[4]}/>
-                                </TableWrapper>))
-                        }
-                    </Table>
-                </View>
+                <Text>
+                    Usługi IT
+                </Text>
             </View>
         )
     }
 }
-
-const styles = StyleSheet.create({
-    container: {flex: 1, padding: 5, paddingTop: 30, backgroundColor: '#fff'},
-    head: {height: 40, backgroundColor: '#808B97'},
-    text: {margin: 2, textAlign: 'center'},
-    row: {flexDirection: 'row', backgroundColor: '#E7E6E1'},
-});
--- src/screens/StateofServicesScreen.js
+++ src/screens/StateofServicesScreen.js
@@ -13,24 +13,9 @@ export default class StateofServicesScreen extends Component {
 
     constructor(props) {
         super(props);
-        this.state = {
-            tableHead: ['Head', 'Head2', 'Head3', 'Head4', 'Head5', 'Head6', 'Head7', 'Head8', 'Head9'],
-            widthArr: [40, 60, 80, 100, 120, 140, 160, 180, 200],
-
-        };
     }
 
     render() {
-        const state = this.state;
-        const tableData = [];
-        for (let i = 0; i < 30; i += 1) {
-            const rowData = [];
-            for (let j = 0; j < 9; j += 1) {
-                rowData.push(`${i}${j}`);
-            }
-            tableData.push(rowData);
-        }
-
         return (
 
             <View style={styles.container}>
@@ -38,27 +23,9 @@ export default class StateofServicesScreen extends Component {
                 <View style={styles.filters}>
                 </View>
                 <ScrollView horizontal={true}>
-                    <View>
-                        <Table borderStyle={{borderColor: '#C1C0B9'}}>
-                            <Row data={state.tableHead} widthArr={state.widthArr} style={styles.header}
-                                 textStyle={styles.text}/>
-                        </Table>
-                        <ScrollView style={styles.dataWrapper}>
-                            <Table borderStyle={{borderColor: '#C1C0B9'}}>
-                                {
-                                    tableData.map((rowData, index) => (
-                                        <Row
-                                            key={index}
-                                            data={rowData}
-                                            widthArr={state.widthArr}
-                                            style={[styles.row, index % 2 && {backgroundColor: '#F7F6E7'}]}
-                                            textStyle={styles.text}
-                                        />
-                                    ))
-                                }
-                            </Table>
-                        </ScrollView>
-                    </View>
+                    <Text>
+                        Stany usług
+                    </Text>
                 </ScrollView>
             </View>
 
