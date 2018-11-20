77d0caf46dfe4424da43e5a7114051127b010974
ACP\Mateusz.Miasik <mateusz.miasik@asseco.pl>
2018-11-20 08:57:26 +0100

    push

--- App.js
+++ App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import {Text, View, SafeAreaView, ScrollView, StatusBar, Button} from 'react-native';
 import {createDrawerNavigator, DrawerItems} from 'react-navigation';
-import { Permissions, Notifications } from 'expo';
+import {Permissions, Notifications} from 'expo';
 
 import ServicesScreen from "./src/screens/SettingsScreen";
 import HomeScreen from "./src/screens/HomeScreen";
@@ -15,6 +15,7 @@ import TableFilters from "./src/components/filters/tableFilters";
 import ServicesDetails from "./src/screens/ServicesDetails";
 
 import TreeScreen from "./src/screens/TreeScreen";
+import LoginScreen from "./src/screens/LoginScreen";
 
 export default class App extends React.Component {
 
@@ -30,8 +31,9 @@ export default class App extends React.Component {
         );
     }
 }
+
 async function registerForPushNotificationsAsync() {
-    const { status: existingStatus } = await Permissions.getAsync(
+    const {status: existingStatus} = await Permissions.getAsync(
         Permissions.NOTIFICATIONS
     );
     let finalStatus = existingStatus;
@@ -41,7 +43,7 @@ async function registerForPushNotificationsAsync() {
     if (existingStatus !== 'granted') {
         // Android remote notification permissions are granted during the app
         // install, so this will only ask on iOS
-        const { status } = await Permissions.askAsync(Permissions.NOTIFICATIONS);
+        const {status} = await Permissions.askAsync(Permissions.NOTIFICATIONS);
         finalStatus = status;
     }
 
@@ -53,9 +55,10 @@ async function registerForPushNotificationsAsync() {
     // Get the token that uniquely identifies this device
     let token = await Notifications.getExpoPushTokenAsync();
 }
+
 const Navigator = createDrawerNavigator(
     {
-
+        'Login': LoginScreen,
         'Strona Główna': HomeScreen,
         'Alerty': CardAlertsScreen,
         'Stany Usług IT': StateofServicesScreen,
@@ -64,7 +67,7 @@ const Navigator = createDrawerNavigator(
         'Tabela Alerty': AlertsScreen,
         'Filters': TableFilters,
         'Szczegóły usług IT': ServicesDetails,
-        'Usługi IT - drzewko':TreeScreen
+        'Usługi IT - drzewko': TreeScreen
     },
     {
         contentComponent: DrawerComponent
